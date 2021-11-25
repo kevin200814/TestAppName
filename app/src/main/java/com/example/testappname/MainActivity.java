f61a4740e6122d6a3d7ead08716e248f864082bd
Kev <82066526+kevin200814@users.noreply.github.com>
2021-11-25 16:56:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/testappname/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testappname;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
